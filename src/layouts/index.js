0fef090c66490cc13f4492224c35d88484d2c2c5
Nikhil Arora <narora200@gmail.com>
2018-09-04 19:21:43 +0530

    [Design]: index page redesigned

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { graphql } from 'graphql';
+import Media from 'react-media';
 
-import Header from '../components/header';
 import './index.css';
 import '../styles/layout-override.css';
+
+import Header from '../components/header';
+import Sidebar from '../components/sidebar';
 
 
 const Layout = ({ children, data }) => (
@@ -13,20 +16,70 @@ const Layout = ({ children, data }) => (
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content: 'Official blog of @iamoperand' },
+        { name: 'keywords', content: 'iamoperand, Nikhil Arora' },
       ]}
     />
     <Header siteTitle={data.site.siteMetadata.title} />
     <div
       style={{
         margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
+        maxWidth: 980,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        height: '100%',
       }}
     >
-      {children()}
+      <Media query={{ maxWidth: 848 }}>
+        {
+          matches =>
+            (matches
+              ? (
+                <div
+                  style={{
+                    margin: '0 auto',
+                    maxWidth: 980,
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    height: '100%',
+                    padding: '25px',
+                  }}
+                >
+                  <div style={{ flex: 1 }}>{children()}</div>
+                </div>
+              )
+              : (
+                <div
+                  style={{
+                    margin: '0 auto',
+                    maxWidth: 980,
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    height: '100%',
+                    padding: '25px',
+                  }}
+                >
+                  <div style={{ flex: 2.5, paddingRight: '30px' }}>
+                    {children()}
+                  </div>
+                  <div style={{ flex: 1 }}>
+                    <Sidebar
+                      title="WebStack"
+                      description="Articles on React and Node.js. All articles are written by Me."
+                    />
+                    <Sidebar
+                      title="About author"
+                      description="I am a Full-stack Web Developer specializing in React and Node.js based in India."
+                    />
+                  </div>
+                </div>
+              )
+            )
+        }
+      </Media>
     </div>
   </div>
 );
